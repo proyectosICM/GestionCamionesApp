1d9d1e33f7a5bcddb4e19cefaba3c6cd2d682310
Eduardo Aguilar <es123y123@gmail.com>
2023-08-02 17:02:59 -0500

    Cambio orden botones

--- Screens/VIstaConductor/RegistarCambioLlantas/CambiarCamion.jsx
+++ Screens/VIstaConductor/RegistarCambioLlantas/CambiarCamion.jsx
@@ -1,41 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native";
 import { Button } from "react-native-elements";
 import { styles } from "../../../Styles/General";
+import { ScrollView } from "react-native";
 
 export function CambiarCamion() {
+  const rows = [
+    [1, , 2],
+    [[3], 4, [5], 6],
+    [[7], 8, [9], 10],
+  ];
+
+  const [cambio, setCambio] = useState(false);
+
+  const handleCambiar = (n) => {
+    setCambio(true);
+  };
   return (
-    <>
+    <ScrollView>
       <Text style={styles.tittleText}>Camion</Text>
-      <View style={localStyles.buttonRow}>
-        <Button title={"1"} buttonStyle={localStyles.button} />
-        <View style={localStyles.buttonSeparator} />
-        <Button title={"2"} buttonStyle={localStyles.button} />
-      </View>
-
-      <View style={localStyles.buttonRow}>
-        <Button title={"3"} buttonStyle={localStyles.button} />
-        <Button title={"4"} buttonStyle={localStyles.button} />
-
-        <View style={localStyles.buttonSeparator} />
-
-        <Button title={"5"} buttonStyle={localStyles.button} />
-        <Button title={"6"} buttonStyle={localStyles.button} />
-      </View>
-
-      <View style={localStyles.buttonRow}>
-        <Button title={"7"} buttonStyle={localStyles.button} />
-        <Button title={"8"} buttonStyle={localStyles.button} />
-
-        <View style={localStyles.buttonSeparator} />
-
-        <Button title={"9"} buttonStyle={localStyles.button} />
-        <Button title={"10"} buttonStyle={localStyles.button} />
-      </View>
-      <Button title={"RP"} buttonStyle={localStyles.button} />
-    </>
+      {rows.map((row, rowIndex) => (
+        <View key={rowIndex} style={localStyles.buttonRow}>
+          {row.map((number) => (
+            <React.Fragment key={number}>
+              {Array.isArray(number) ? (
+                <>
+                  {number.map((nestedNumber) => (
+                    <React.Fragment key={nestedNumber}>
+                      <Button
+                        title={String(nestedNumber)}
+                        buttonStyle={localStyles.button}
+                        onPress={() => handleCambiar(nestedNumber)}
+                      />
+                      {number.indexOf(nestedNumber) < number.length - 1 && (
+                        <View style={localStyles.buttonSeparator} />
+                      )}
+                    </React.Fragment>
+                  ))}
+                </>
+              ) : (
+                <>
+                  <Button
+                    title={String(number)}
+                    onPress={() => handleCambiar(number)}
+                    buttonStyle={localStyles.button}
+                  />
+                  {row.indexOf(number) < row.length - 1 && (
+                    <View style={localStyles.buttonSeparator} />
+                  )}
+                </>
+              )}
+            </React.Fragment>
+          ))}
+        </View>
+      ))}
+      <View style={localStyles.buttonSeparator} />
+      <Button
+        title={"RP"}
+        buttonStyle={localStyles.button}
+        onPress={() => handleCambiar("RP")}
+      />
+      <View style={localStyles.buttonSeparator} />
+      {cambio && (
+        <Text style={[styles.tittleText, { margin: 10 }]}>
+          Registro de cambio
+        </Text>
+      )}
+    </ScrollView>
   );
 }
 
--- Screens/VIstaConductor/RegistarCambioLlantas/RegistrarCambioLlantas.jsx
+++ Screens/VIstaConductor/RegistarCambioLlantas/RegistrarCambioLlantas.jsx
@@ -6,12 +6,13 @@ import { Button } from "react-native-elements";
 import { CambiarCamion } from "./CambiarCamion";
 import { useState } from "react";
 import { CambiarCarreta } from "./CambiarCarreta";
+import { ScrollView } from "react-native";
 
 export function RegistrarCambioLlantas() {
   const [mostrar, setMostrar] = useState(null);
 
   const SelectTC = (op) => {
-    if ((op == "camion")) {
+    if (op == "camion") {
       setMostrar("camion");
     } else if (op == "carreta") {
       setMostrar("carreta");
@@ -20,31 +21,36 @@ export function RegistrarCambioLlantas() {
 
   return (
     <View style={styles.container}>
-      <Button
-        title={"Camion"}
-        titleStyle={styles.tittleText}
-        buttonStyle={styles.styleButton}
-        onPress={() => SelectTC("camion")}
-      />
+        <Button
+          title={"Camion"}
+          titleStyle={styles.tittleText}
+          buttonStyle={styles.styleButton}
+          onPress={() => SelectTC("camion")}
+        />
 
-      <Button
-        title={"Carreta"}
-        titleStyle={styles.tittleText}
-        buttonStyle={styles.styleButton}
-        onPress={() => SelectTC("carreta")}
-      />
-      {mostrar != null && (
-        <>
-          <Text style={styles.tittleText}>Cambio de llantas</Text>
-          <Text style={styles.tittleText}>
-            Seleccione la llanta cambiada en ruta {mostrar}
-          </Text>
+        <Button
+          title={"Carreta"}
+          titleStyle={styles.tittleText}
+          buttonStyle={styles.styleButton}
+          onPress={() => SelectTC("carreta")}
+        />
+        {mostrar != null && (
+          <>
+            <Text style={styles.tittleText}>Cambio de llantas</Text>
+            <Text style={styles.tittleText}>
+              Seleccione la llanta cambiada en ruta {mostrar}
+            </Text>
 
-          {mostrar === "camion" ? <CambiarCamion /> : mostrar === "carreta" ? <CambiarCarreta /> : <Text>Seleccione un boton</Text>}
-        </>
-      )}
+            {mostrar === "camion" ? (
+              <CambiarCamion />
+            ) : mostrar === "carreta" ? (
+              <CambiarCarreta />
+            ) : (
+              <Text>Seleccione un boton</Text>
+            )}
+          </>
+        )}
 
-    
     </View>
   );
 }
