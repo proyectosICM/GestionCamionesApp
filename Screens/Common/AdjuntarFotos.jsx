c42d0bfefdbd9ee4cc90e0d2d927ac9be0a00bf6
Eduardo Aguilar <es123y123@gmail.com>
2023-08-08 13:53:45 -0500

    Trabajando Fotos

--- Screens/Common/AdjuntarFotos.jsx
+++ Screens/Common/AdjuntarFotos.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, TextInput } from "react-native";
 import { View } from "react-native";
 import { styles } from "../../Styles/General";
 import { Button } from "react-native-elements";
@@ -10,9 +10,29 @@ import { Image } from "react-native";
 import axios from "axios";
 import { useAgregarElemento } from "../../Hooks/CRUDHook";
 import { FallasImagenURL } from "../../API/apiurl";
+import { StyleSheet } from "react-native";
+import { useCallback } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export function AdjuntarFotos() {
+
+  const [usuario, setUsuario] = useState();
+
+
+
+  const datosAsync = useCallback(async () => {
+    const usuariov = await AsyncStorage.getItem("usuario");
+    setUsuario(usuariov);
+
+  }, []);
+
+  useEffect(() => {
+    datosAsync();
+  }, [datosAsync]);
+
+
   const [image, setImage] = useState(null);
+  const [observacion, setObservacion] = useState(null);
   const userAccessToken = "e7eefedec49196bf8229bedb7324c29d72ef4bd3";
 
   const handleImagePicker = async () => {
@@ -56,22 +76,23 @@ export function AdjuntarFotos() {
           // La imagen se ha subido exitosamente.
           const uploadedImageUrl = response.data.data.link;
           console.log("Imagen subida con éxito:", uploadedImageUrl);
-/*
+
           const requestData = {
-            id: 1,
             urlImage: uploadedImageUrl,
-            observacion: "Dañado 2",
+            observacion: observacion,
             usuariosModel: {
-                id: 1
+              id: usuario,
             },
             rgsModel: {
-                id: 1
-            }
-          }
-
+              id: 1,
+            },
+          };
+          console.log(requestData);
+          console.log(FallasImagenURL);
           await useAgregarElemento(FallasImagenURL, requestData);
-*/
+
           setImage(null);
+          setObservacion("");
         } else {
           console.log("Error al subir la imagen:", response.data);
         }
@@ -91,18 +112,72 @@ export function AdjuntarFotos() {
         <>
           <Image
             source={{ uri: image }}
-            style={styles.image}
+            style={styles2.image}
             resizeMode="contain"
           />
-          <Button title={"Enviar"} onPress={handleEnviar} />
+          <Text style={[styles.tittleText, { margin: 2 }]}>
+            Agrege la observacion
+          </Text>
+          <TextInput
+            style={styles2.input}
+            placeholder="Nombre de usuario"
+            onChangeText={(text) => setObservacion(text)}
+            value={observacion}
+          />
+          <Button
+            title={"Enviar"}
+            onPress={handleEnviar}
+            buttonStyle={styles.styleButton}
+            titleStyle={styles.tittleText}
+          />
         </>
       ) : (
-        <View style={styles.previewContainer}>
-          <Button title="Tomar Foto" onPress={handleImagePicker} />
+        <View style={styles2.previewContainer}>
+          <Button
+            title="Tomar Foto"
+            onPress={handleImagePicker}
+            buttonStyle={styles.styleButton}
+            titleStyle={styles.tittleText}
+            icon={{
+              name: "camera",
+              type: "font-awesome",
+              size: 25,
+              color: "white",
+            }}
+          />
         </View>
       )}
-      <Button title={"Ver imagen"} onPress={() => handleVerImagen()} />
       {/* zona para la imagen */}
     </View>
   );
 }
+
+const styles2 = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  previewContainer: {
+    width: "80%",
+    height: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+  },
+  image: {
+    width: "50%",
+    height: "50%",
+  },
+  input: {
+    width: 240, // Ancho del 80% del contenedor
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+});
