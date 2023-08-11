347a2ecaa2a43ce57968efd9d52fa010592ba661
Eduardo Aguilar <es123y123@gmail.com>
2023-08-11 10:55:34 -0500

    Inicio conductor cambiado

--- Screens/VIstaConductor/VerificacionCamion.jsx
+++ Screens/VIstaConductor/VerificacionCamion.jsx
@@ -1,16 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useState } from "react";
-import { Touchable } from "react-native";
-import QRScanner from "../../QRScanner";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { styles } from "../../Styles/General";
 import { Button } from "react-native-elements";
 import { ImageBackground } from "react-native";
+import QRScanner from "../../QRScanner";
+import { styles } from "../../Styles/General";
 import { ColorIcono, ColorTexto, fondo } from "../../Styles/PaletaColores";
+
 const fondo1 = require("../../Styles/fondo5.jpg");
-const qr = require("../../Styles/icmLogo.png")
+const qr = require("../../Styles/icmLogo.png");
+
 export default function VerificacionCamion({ navigation }) {
   const [abrir, setAbrir] = useState(false);
   const handleAbrirCamera = () => {
@@ -19,7 +17,7 @@ export default function VerificacionCamion({ navigation }) {
 
   const handleCerrarCamera = () => {
     setAbrir(false);
-  }; 
+  };
 
   return abrir ? (
     <QRScanner
@@ -29,26 +27,38 @@ export default function VerificacionCamion({ navigation }) {
     />
   ) : (
     <ImageBackground source={fondo} style={styles.backgroundImage}>
-    <View style={styles.container}>
-      <Image source={qr} style={{width:100, height:100, marginVertical: 20}}/>
-      <Text style={styles.tittleText}>Escanear QR de camion</Text>
-      <Text style={{textAlign:"center", fontSize: 20, fontWeight: "bold", marginVertical: 12, color: ColorTexto}}>Escanee el codigo QR de un camion para empezar a realizar el CheckList</Text>
-      <Button
-        title=" Abrir Camara "
-        type="outline"
-        buttonStyle={styles.buttonPalette}
-        titleStyle={styles.textoButton}
-        /*icon={<FontAwesome name="camera" size={20}  color="red" />}*/
-        icon={{
-          name: "camera",
-          type: "font-awesome",
-          size: 25,
-          color: ColorIcono,
-        }}
-        iconRight
-        onPress={handleAbrirCamera}
-      />
-    </View>
+      <View style={styles.container}>
+        <Image
+          source={qr}
+          style={{ width: 100, height: 100, marginVertical: 20 }}
+        />
+        <Text style={styles.tittleText}>Escanear QR de camion</Text>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 20,
+            fontWeight: "bold",
+            marginVertical: 12,
+            color: ColorTexto,
+          }}
+        >
+          Escanee el codigo QR de un camion para empezar a realizar el CheckList
+        </Text>
+        <Button
+          title=" Abrir Camara "
+          type="outline"
+          buttonStyle={styles.buttonPalette}
+          titleStyle={styles.textoButton}
+          icon={{
+            name: "camera",
+            type: "font-awesome",
+            size: 25,
+            color: ColorIcono,
+          }}
+          iconRight
+          onPress={handleAbrirCamera}
+        />
+      </View>
     </ImageBackground>
   );
 }
